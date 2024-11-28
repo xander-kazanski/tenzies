ee8c497bc66c44ecb6e8a70dc99bcb05dfb123e7
Alexander Kazanski <alexanderkazanski@outlook.com>
2024-11-28 12:15:15 -0500

    fix deploy

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import {defineConfig} from "vite"
+import react from "@vitejs/plugin-react"
+
+export default defineConfig({
+	plugins: [
+		react()
+	],
+  esbuild: {
+    loader: 'jsx',
+    include: /.*\.jsx?$/,
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
+})
